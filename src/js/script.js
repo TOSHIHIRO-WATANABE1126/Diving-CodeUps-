8f8212f50d6b57a201e9eec092b4733d86d73ff5
Toshi <sleepwalker1126@hotmail.co.jp>
2025-02-07 09:08:45 +0900

    3回目(header完了)

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,8 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
-
+  $(".js-hamburger,.js-drawer").click(function () {
+    $(".js-hamburger").toggleClass("is-active");
+    $(".js-drawer").fadeToggle();
+  });
 
 });
