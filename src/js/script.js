5e3356b5b0271d29001c0c939d8087a84c47e4b7
Toshi <sleepwalker1126@hotmail.co.jp>
2025-02-09 08:21:29 +0900

    ４回目（campaign鑑賞）

--- src/js/script.js
+++ src/js/script.js
@@ -4,5 +4,37 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
     $(".js-hamburger").toggleClass("is-active");
     $(".js-drawer").fadeToggle();
   });
+});
 
+// var mvSwiper = new Swiper(".js-mv-swiper", {
+//   loop: true,
+//   effect: "fade",
+//   speed: 3000,
+//   allowTouchMove: false,
+//   autoplay: {
+//     delay: 3000,
+//   },
+// });
+
+var campaignSwiper = new Swiper(".js-campaign-swiper", {
+  navigation:{
+   nextEl:".campaign__button-next",
+   prevEl:".campaign__button-prev",
+  },
+  loop: true,
+  effect: "card",
+  speed: 300,
+  spaceBetween: 24,
+  slidesPerView: 1.2,
+  breakpoints:{
+    768:{
+      spaceBetween:40,
+      slidesPerView: 3.05,
+    }
+  },
+  // centeredSliders: true,
+  // allowTouchMove: false,
+  // autoplay: {
+  //   delay: 3000,
+  // },
 });
