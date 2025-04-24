82e143eca25c1fd88641daa6e7d26bc6755192f1
Toshi <sleepwalker1126@hotmail.co.jp>
2025-04-24 14:27:48 +0900

    13回目（zoom予約前）

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,6 @@
 
 jQuery(function ($) {
-  // `$` を jQuery オブジェクトとして安全に使用できるようにする
+  // この中であればWordpressでも「$」が使用可能になる
 
   // ハンバーガーメニュー (`.js-hamburger`) またはドロワーメニュー (`.js-drawer`) がクリックされたときの処理
   $(".js-hamburger, .js-drawer").click(function () {
@@ -9,10 +9,32 @@ jQuery(function ($) {
     // → メニューの開閉状態を視覚的に切り替えるためのクラス
     $(".js-hamburger, .js-header").toggleClass("is-active");
 
+    if ($(".js-hamburger").hasClass("is-active")) {
+      $("body").css("overflow", "hidden");
+    } else {
+      $("body").css("overflow", "auto");
+    }
+
     // `.js-drawer` をフェードイン・フェードアウト（ゆっくり表示・非表示）を切り替える
     $(".js-drawer").fadeToggle();
   });
 });
+
+  //pc画面幅ではドロワーメニューを非表示にする
+  // ウィンドウのサイズが変更されたときに、この関数が実行される
+ $(window).resize(function () {
+
+   // もしウィンドウの横幅が768ピクセル以上（=PCサイズの画面）だったら
+   if ($(window).width() >= 768) {
+
+     // ドロワーメニュー（スマホ用メニュー）を閉じる：クラス「is-active」を削除し、スタイルを初期化する
+     $(".js-drawer").removeClass("is-active").css("display", "");
+
+     // ヘッダーの開閉状態をリセット：クラス「is-active」を削除し、スタイルを初期化する
+     $(".js-header").removeClass("is-active").css("display", "");
+   }
+ });
+
 
 var mvSwiper = new Swiper(".js-mv-swiper", {
   loop: true,
